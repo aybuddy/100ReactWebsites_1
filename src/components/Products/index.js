34aa7c4f9c56edfa617911200627d7a129d5fd85
Aaron Yu <ayybuddyy@gmail.com>
2021-02-22 21:05:31 -0500

    Add products section, add products data and map over data

---
+++ src/components/Products/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  ProductsContainer,
+  ProductWrapper,
+  ProductHeading,
+  ProductCard,
+  ProductImg,
+  ProductInfo,
+  ProductDesc,
+  ProductPrice,
+  ProductButton,
+} from './Products.styles.js';
+
+const Products = ({ heading, data }) => {
+  return (
+    <ProductsContainer>
+      <ProductHeading>Heading</ProductHeading>
+      <ProductWrapper>
+        {data.map((product, index) => {
+          return (
+            <ProductCard key={index}>
+              <ProductImg src={product.img} alt={product.alt} />
+              <ProductInfo>{product.name}</ProductInfo>
+              <ProductDesc>{product.desc}</ProductDesc>
+              <ProductPrice>{product.price}</ProductPrice>
+              <ProductButton>{product.button}</ProductButton>
+            </ProductCard>
+          );
+        })}
+      </ProductWrapper>
+    </ProductsContainer>
+  );
+};
+
+export default Products;
