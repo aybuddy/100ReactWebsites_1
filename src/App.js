e73bbc2689d5b6e6a86aa08a5d2ca3fda76d73b2
Aaron Yu <ayybuddyy@gmail.com>
2021-02-22 21:13:02 -0500

    Add features section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { GlobalStyle } from './globalStyles';
 import Hero from './components/Hero';
 import { productData } from './components/Products/data';
 import Products from './components/Products';
+import Feature from './components/Feature';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <GlobalStyle />
       <Hero />
       <Products heading='Choose your favorite' data={productData} />
+      <Feature />
     </Router>
   );
 }
